9530edc83fc3ac8e499fca8386cac369acd410b7
Muhammad Rizky Hidayah Akbar <93087447+Rizbar@users.noreply.github.com>
2021-11-16 10:36:25 +0700

    Update Jadwal.java

--- Jadwal.java
+++ Jadwal.java
@@ -1,8 +1,29 @@
 /**
  * 
- * @author
- * @version
+ * @author (NEW-OMEGA)
+ * @version (15-11-2021)
  */
+
 public class Jadwal {
+    private String hari;
+    private int jam;
+    
+    public Jadwal(){
+    }
+    public void setHari(String hari){
+        this.hari = hari;
+    }
+
+    public String getHari(){
+        return hari;
+    }
+
+    public void setJam(int jam){
+        this.jam = jam;
+    }
+
+    public int getJam(){
+        return jam;
+    }
     
 }
