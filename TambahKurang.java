e4b87e6009f30664e8253e20355821c9b528bbe3
Muhammad Rizky Hidayah Akbar <93087447+Rizbar@users.noreply.github.com>
2021-12-01 11:38:30 +0700

    done

--- TambahKurang.java
+++ TambahKurang.java
@@ -1,8 +1,19 @@
+/**
+ * Interface untuk menambah dan mengurangi integer
+ * @author (NEW-OMEGA)
+ * @version (01-12-2021)
+ */
 public interface TambahKurang
 {
-    
+    /**
+     * Method untuk menambahkan nilai.
+     * @param nilai
+     */
     void tambah(int nilai);
     
-
+    /**
+     * Method untuk mengurangi nilai.
+     * @param nilai
+     */
     void kurangi(int nilai);
 }
